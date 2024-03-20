2d4b236482fe8129f0c76f1517ec7203b170b690
frogcodes <147776756+frogcodes@users.noreply.github.com>
2024-03-19 20:17:58 -0400

    lineup 2.1

--- src/main/java/frc/robot/commands/auto/ConnorLineup.java
+++ src/main/java/frc/robot/commands/auto/ConnorLineup.java
@@ -6,47 +6,50 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Swerve;
 import swervelib.SwerveDrive;
 
 // This command aligns the robot based on feedback from the limelight camera
-public class ConnorLineup extends CommandBase {
+public class ConnorLineup extends Command {
 
     private NetworkTable limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
     private NetworkTableEntry horizontalOffset = limelightTable.getEntry("tx");
     private NetworkTableEntry verticalOffset = limelightTable.getEntry("ty");
     private Swerve drive = Swerve.getInstance();
-    SwerveDrive swerveDrive;
-
-    private double rotateX = Math.cos(Math.toRadians(30));
-     private double rotateY = Math.sin(Math.toRadians(30));
-    private Translation2d rightMovement = new Translation2d(0.0, -0.2);
-    private Translation2d leftMovement = new Translation2d(0.0, 0.2);
-    private Translation2d forwardMovement = new Translation2d(-0.2, 0);
-    private Translation2d backMovement = new Translation2d(0.2, 0);
-    private Translation2d rotateClockwise = new Translation2d(rotateX , rotateY); // 30 degrees clockwise
-    private Translation2d rotateNotClockwise = new Translation2d(rotateX , -rotateY); // 30 degrees counterclockwise (or anticlockwise)
     
+
+   private double rotateX = Math.cos(Math.toRadians(30));
+   private double rotateY = Math.sin(Math.toRadians(30));
+    private Translation2d rightMovement = new Translation2d(0.0, -0.25);
+    private Translation2d leftMovement = new Translation2d(0.0, 0.25);
+   private Translation2d forwardMovement = new Translation2d(-0.25, 0);
+   private Translation2d backMovement = new Translation2d(0.25, 0);
+   //private Translation2d rotateClockwise = new Translation2d(rotateX , rotateY); // 30 degrees clockwise
+   //private Translation2d rotateNotClockwise = new Translation2d(rotateX , -rotateY); // 30 degrees counterclockwise (or anticlockwise)
+    private Translation2d noMove = new Translation2d(0,0);
+    double rotateClockwise = 0.1;
+    double rotateNotClockwise = -0.1;
+
     private double OFFSET_THRESHOLD = 0.01;
-    private double vertOFFSET_THRESHOLD = 0.1;
-    private double angOFFSET_THRESHOLD = 0.1;
+    private double vertOFFSET_THRESHOLD = 0.01;
+   private double angOFFSET_THRESHOLD = 0.1;
 
     private double kLimeLightVerticalAngle = verticalOffset.getDouble(0);
-    private double kTargetDistance = 48.0;
-     private double kLimeLightAngle = 0;
-     private double kAprilTagHeight = 2.032;
-     private double kLimelightHeight = 0;
-     private double kLimelightDiff = 0;
-     private double kDistance = 0;
+    private double kTargetDistance = 1.2192;
+     private double kLimeLightAngle = 45.0;
+     private double kAprilTagHeight = 1.2192;
+     private double kLimelightHeight = 0.597;
+     private double kLimelightDiff;
+     private double kDistance;
      private double aprilTagID = 0;
-      private Rotation2d rawHeading = swerveDrive.getOdometryHeading();
-     private double heading = rawHeading.getDegrees();
+     private Rotation2d rawHeading = Swerve.getInstance().getGyroAngle();
+    private double heading = rawHeading.getDegrees();
     
      
-    
+  
 
     int atagIDConvert(){
         aprilTagID = limelightTable.getEntry("tid").getDouble(-1);
@@ -83,15 +86,16 @@ public class ConnorLineup extends CommandBase {
     @Override
     public void execute() {
         // If there's a target and the horizontal offset is significant, adjust the alignment
-        if (limelightTable.getEntry("tv").getDouble(0) != 0) {
-            aprilTagID = limelightTable.getEntry("tid").getDouble(-1);
+       if (limelightTable.getEntry("tv").getDouble(0) != 0) {
+             aprilTagID = limelightTable.getEntry("tid").getDouble(-1);
             System.out.println("April Tag ID:" + atagIDConvert());
             while (Math.abs(idealHeading - heading) > angOFFSET_THRESHOLD){
                 System.out.println("Updated Heading:" + heading);
-                Translation2d rotate = (heading > idealHeading) ? rotateNotClockwise : rotateClockwise;
-                drive.drive(rotate,0.0,false);
+                //Translation2d rotate = (heading > idealHeading) ? rotateNotClockwise : rotateClockwise;
+                double rotate = (heading > idealHeading) ? rotateNotClockwise : rotateClockwise;
+                drive.drive(noMove,rotate,false);
                 heading = rawHeading.getDegrees();
-            }
+            } 
 
             double aprilTagOffset = horizontalOffset.getDouble(0);
             System.out.println("Offset: " + aprilTagOffset);
@@ -101,15 +105,19 @@ public class ConnorLineup extends CommandBase {
                 drive.drive(movement, 0.0, false);
                 aprilTagOffset = horizontalOffset.getDouble(0);
             }
-        
+            System.out.println("Time to Vertical Lineup");
+            kLimelightDiff = (kAprilTagHeight - kLimelightHeight);
+            kDistance = kLimelightDiff/Math.atan(kLimeLightAngle + kLimeLightVerticalAngle);
+            System.out.println("Distance:" + kDistance );
         while (Math.abs(kTargetDistance - kDistance) > vertOFFSET_THRESHOLD) {
             System.out.println("Distance:" + kDistance );
-            Translation2d yAxisMovement = (kLimeLightVerticalAngle < 0) ? forwardMovement : backMovement;
+            Translation2d yAxisMovement = (kLimeLightVerticalAngle < 0) ? forwardMovement: backMovement;
+            System.out.println(yAxisMovement);
             drive.drive(yAxisMovement, 0.0, false);
-            kLimelightDiff = (kAprilTagHeight - kLimelightHeight);
             kDistance = kLimelightDiff/Math.tan(kLimeLightAngle + kLimeLightVerticalAngle);
             kLimeLightVerticalAngle = verticalOffset.getDouble(0);
         } 
+        System.out.println("Lineup Complete");
     }   
     else {
             System.out.println("No April Tag Detected");
