3423794cba5e3ae55b90c3fa0d75482ee4c329d7
MysteriousEmperor <>
2024-03-30 11:23:01 -0400

    More code filler

--- src/main/java/frc/robot/subsystems/ClimberTesting.java
+++ src/main/java/frc/robot/subsystems/ClimberTesting.java
@@ -1,5 +1,41 @@
 package frc.robot.subsystems;
 
-public class ClimberTesting {
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
+
+import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.WiredSubsystem.InnerWiredSubsystemState;
+
+public class ClimberTesting extends SubsystemBase implements WiredSubsystem {
+    
+    static Climber instance;
+
+    public enum ClimberState implements InnerWiredSubsystemState {
+        HOME,
+        LOCKED,
+        EXTENDING,
+        RETRACTING,
+    } 
+
+    ClimberState climberState;
+
+    CANSparkMax winchMotor;
+    SparkPIDController winchPID;
+
+    PWM releaseServo;
+
+    public ClimberTesting() {
+
+        climberState = ClimberState.HOME;
+
+        winchMotor = new CANSparkMax(Constants.ClimberConstants.climberMotorID, MotorType.kBrushless);
+        releaseServo = new PWM(Constants.ClimberConstants.servoID);
+    }
+    public void setState(ClimberState state) {
+        climberState = state;
+}
 
 }
